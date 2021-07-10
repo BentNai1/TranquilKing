dcfa491a619489bf0c8b1441b4f628dde0fb8630
BentNai1 <eattopramen@hotmail.com>
2021-07-10 11:55:25 -0400

    All basic hookups for interaction
    buttons, text, and sprite all have function scripts

---
+++ Tranquil King/Assets/ButtonController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour
+{
+    public Button[] button;
+
+    void Start()
+    {
+        ChangeButton(0, true, "Hello, this is a test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeButton(int WhichButton, bool stateIsOn, string buttonText)
+    {
+        button[WhichButton].enabled = stateIsOn;
+        button[WhichButton].GetComponentInChildren<Text>().text = buttonText;
+    }
+}
---
+++ Tranquil King/Assets/TextController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour
+{
+
+    public Text speakerTextObject;
+    public Text speakingTextObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeSpeaker("Test");
+        ChangeSpeakingText("This is a test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeSpeaker(string speakerName)
+    {
+        speakerTextObject.text = speakerName;
+    }
+
+    public void ChangeSpeakingText(string textToDisplay)
+    {
+        speakingTextObject.text = textToDisplay;
+    }
+}
